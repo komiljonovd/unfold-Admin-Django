56729a9b77f8149e11d5f2b15ceec8614b5a8aa8
komiljonoovd <komiljonooov.d@gmail.com>
2025-02-17 11:17:41 +0500

    committed

--- unfoldapp/admin.py
+++ unfoldapp/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
 from unfold.contrib.forms.widgets import WysiwygWidget
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
+
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+from unfold.admin import ModelAdmin
 
 from unfoldapp.models import Report
-from unfold.admin import ModelAdmin
-from django.db import models
-
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
+
 
 @admin.register(Report)
 class ReportAdmin(ModelAdmin):
@@ -16,4 +34,3 @@ class ReportAdmin(ModelAdmin):
     search_fields = ('name', 'description', 'isdeleted', 'createdon', 'modifiedon', 'createdby', 'modifiedby')
     ordering = ('id',)
     list_per_page = 30
-
