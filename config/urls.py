ff3545c16dcf5dd6c607bf9ea0623cede6c9ce97
komiljonovd <komiljonooov.d@gmail.com>
2025-09-16 23:43:45 +0500

    fix

--- config/urls.py
+++ config/urls.py
@@ -19,22 +19,22 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
-# admin.site.site_header = 'KDA'
-# admin.site.site_title = 'Abdurakhimovich'
+
+
 admin.site.index_title = 'WELCOME TO ADMINISTRATION'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', lambda request: redirect('admin/', permanent=False), ),
-
-
-
-
-
-
+    path("i18n/", include("django.conf.urls.i18n")),
 
 ]
+
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('admin/', permanent=False),),
+
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
